b4537e9c64fb242dffb4a14667f0933f197ba83a
Leonardo <leitee23@hotmail.com>
2019-07-17 13:09:59 -0300

    FE: listado de competidores funcionando

--- src/prisma.spa/src/app/_pages/competidores/competidores.component.ts
+++ src/prisma.spa/src/app/_pages/competidores/competidores.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { PrismaService } from '@/_services';
+import { Observable } from 'rxjs';
+import { Competidor } from '@/_models';
 
 @Component({
   selector: 'app-competidores',
   templateUrl: './competidores.component.html',
-  styleUrls: ['../pages.component.scss']
+  styleUrls: ['../pages.component.scss'],
+  providers: [PrismaService]
 })
 export class CompetidoresComponent implements OnInit {
 
-  constructor() { }
+  private compListAsync: Observable<Array<Competidor>>;
+
+  constructor(private prismaSvc: PrismaService) { }
 
   ngOnInit() {
+    this.compListAsync = this.prismaSvc.getAllCompetidor();
+  }
+
+  update(c: Competidor){
+    console.log(c)
+    return false;
+  }
+
+  delete(c: Competidor){
+    console.log(c)
+    return false;
   }
 
 }
--- src/prisma.spa/src/app/app.component.ts
+++ src/prisma.spa/src/app/app.component.ts
@@ -9,4 +9,7 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
     }
+
+    agregar(): void {
+    }
 }
